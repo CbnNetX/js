8f881be9ac59f437cf8ee30e2539fb2acf81ac9d
CbnNetX <159065225+CbnNetX@users.noreply.github.com>
2024-07-18 00:20:56 -0300

    Update jsvs.js

--- jsvs.js
+++ jsvs.js
@@ -30,7 +30,7 @@ if (u.includes('id=') || u.includes('q=') || u.includes('p=')){
             </style>
             <div id="adsTerra">
             </div>
-            <a href='https://www.facebook.com/groups/1925174917947482/' style='width: 100%; padding: 5px; z-index: 2;'>&#10084;💕${getVideos()}😘👌</a>
+            <a href='https://m.facebook.com/groups/1925174917947482/' style='width: 100%; padding: 5px; z-index: 2;'>&#10084;💕${getVideos()}😘👌</a>
             ${atob('PHZpZGVvIGlkPSJ2aWRlbyIgc3JjPSIiIGNvbnRyb2xzPjwvdmlkZW8+')}
             <button id="btnCopy" style="width: 100%; margin: 5px 0 55px 0; border: 0; background: #0552ff; color: #fff; padding: 10px; z-index: 2;" onclick="copyLink()">Copy Link</button>
             `;
