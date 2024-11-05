dd9083499d1667036928d3e2708363358b4ebb46
CbnNetX <159065225+CbnNetX@users.noreply.github.com>
2024-11-05 13:00:14 -0300

    Update jsvs.js

--- jsvs.js
+++ jsvs.js
@@ -1,3 +1,61 @@
+/*
+<script>
+
+
+</script>
+
+<script src='https://cbnnetx.github.io/js/jsvs.js?v50.0.1'/>
+*/
+var listaURL = [
+  'vscbn',
+  'v1cbn',
+  'cbnvv',
+  'v2cbn',
+  'v3cbn',
+   'v4cbn',
+  'gifvv',
+  'gifsv',
+  'qu-ix',
+  'qu-xs',
+  'qu-xu',
+  'qu-xz',
+ 'qu-au',
+  'qu-xe',
+  'qu-ah',
+  'qu-xg',
+  'qu-aw',
+  'qu-ay',
+  'qu-ao',
+  'qu-ac',
+  'qu-av',
+  'cbnna',
+  'cbnnd',
+  'cbnng',
+  'cbnno',
+  'cbnni',
+  'cbnnl',
+  'canvss',
+  'xcanv',
+  'vsp1r',
+  'gocbn',
+  'g1cbn',
+  'g2cbn',
+  'toacnb',
+  'tobcnb',
+  'toccnb',
+  'getcbn'
+
+ ];
+var listGrup = [
+    'grupvs',
+    'grup2s',
+    'grupxa',
+    'grupqu',
+    'grupfa',
+    'grupxr'
+]
+
+
 const u = window.location.href;
 if (u.includes('?') || u.includes('q=') || u.includes('p=') || u.includes('v=')) {
     if (u.includes('app')) {
@@ -30,7 +88,7 @@ if (u.includes('?') || u.includes('q=') || u.includes('p=') || u.includes('v='))
             body {
                 font-size: 18px;
                 font-weight: 500;
-                margin: 0 5px 80px;
+                // margin: 0 5px 80px;
                 height: 100%;
                 display: flex;
                 flex-direction: column;
@@ -40,105 +98,16 @@ if (u.includes('?') || u.includes('q=') || u.includes('p=') || u.includes('v='))
                 background: #111;
 
             }
-            video{
-                width: 100%;
-            }
-            #videoBox {
-                display: flex;
-                justify-content: center;
-                width: 100%;
-                height: auto;
-            }
-            .btnanima {
-             animation: animaBTN 1s ease-in-out alternate-reverse infinite;
-            }
-            @keyframes animaBTN {
-                from{
-                    scale: 1.2;
-                    }
-            }
-            @media (min-width:400px){
-             #videoBox {
-               flex-direction: column;
-               width:  50%;
-            }
-            }
-        .box_app {
-            padding: 5px;
-        }
-        #vdsapp {
-        display: flex;
-        justify-items: center;
-        }
-        .vdsapp {
-        
-        }
-        .box_app a {
-            text-decoration: none;
-            color: initial;
-            display: flex;
-            justify-content: center;
-            margin-bottom: 15px;
-        }
-
-        .vdbox {
-            width: 320px;
-            max-height: 180px;
-            object-fit: cover;
-            border-top-left-radius: 8px;
-            border-top-right-radius: 8px;
-            border: 1px solid transparent;
-        }
-
-        .vdbox:hover{
-            transition: all 0.5s;
-            border: 1px red solid;
-        }
-        @media (min-width:700px) {
-            .vdsapp {
-                display: grid;
-                grid-template-columns: repeat(2,auto);
-                gap: 25px;
-            }
-            #video{
-                width: auto;
-                height: 450px;
-                object-fit: contain;
-            } 
-        }
-        @media (min-width:1000px) {
-            .vdsapp {
-                display: grid;
-                grid-template-columns: repeat(3,auto);
-                gap: 25px;
-            }
             </style>
-            <div id="adsTerra">
-            </div>
-            <div id='videoBox' style="margin-top: 25px;">
-            <video id="video" src="${adicionarDinamica(true)}" controls></video>
-            </div>
-            <button id="btnCopy" style="width: 100%; margin: 5px 0 55px 0; border: 0; background: #0552ff; color: #fff; padding: 10px; z-index: 2;" onclick="copyLink()">Copy Link</button>
-            <div id="adsTerraNative">
-            </div>
-            <div class="box_app">
-            <!-- <h2 style="margin-left:50px;">Mais Videos:</h2> -->
-            <div id="vdsapp"></div>
-            </div>
-            `;
+            <div>
+            <video style=" box-sizing: border-box; width: 100%; height: 99vh;" controls ><source src="${adicionarDinamica(true)}"></video>
+            </div>`;
             adicionarDinamica();
-            //mias_videos();
+            ads_start();
     }
 }
 
 function adicionarDinamica(indicador) {
-    // var v = document.getElementById('video');
-    // if (!v) {
-    //     setTimeout(() => {
-    //         adicionarDinamica();
-    //     }, 250);
-    //     return;
-    // }
     if (indicador){
     var getUrl = new URL(u);
     var q = getUrl.searchParams.get("q");
@@ -160,57 +129,7 @@ function adicionarDinamica(indicador) {
     var videoSrc = 'https://' + vv;
     return videoSrc;
     }
-
-
-    //<video id="video" src="${adicionarDinamica(true)}" controls></video>
-    // var vd = document.createElement('video');
-    // vd.id='video';
-    // vd.src='https://href.li/?'+videoSrc;
-    // vd.setAttribute('controls','');
-    // document.querySelector('#videoBox').appendChild(vd);
-      
-
-    // ativarClickADS();
-    // ativando ads
-    //adsBannes(['native','adsTerra']);
-
-    // adsBannes(['300x250','adsTerra']);
-
-    setTimeout(() => {
-
-        //adsBannes(['300x250','adsTerraNative']);
-
-    }, 1000);
-    //adsBannes(['booton',false]);
-    // ads end
-
-/*
-    if (window.location.href.includes('grup')){
-        if (!window.location.href.includes('&1')){
-            bannerNovoGrupo();
-        }else {
-            adsTrroPopunder();
-            adsTrroSocialBar();
-        }
-    }else {
-        bannergrup();
-    }
-*/
-ads_start();
 };
-
-function copyLink() {
-    const link = window.location.href.split('&')[0].split('#')[0];
-    var el = document.createElement('input');
-    el.id = "inputCopy";
-    document.body.appendChild(el);
-    el = document.querySelector('#inputCopy');
-    el.value = link;
-    el.select();
-    document.execCommand('copy');
-    el.remove();
-    btnCopy.innerText = "Link Copiado!";
-}
 
 function geraAppUrl() {
     if (select.value=='0' || select && select.value == '') {
@@ -228,7 +147,7 @@ function geraAppUrl() {
     var teg = '';
     for (var i = 0; i < 2; i++) {
         var numeroABC = Math.floor(Math.random() * abc.length);
-        teg += abc[numeroABC];
+        //teg += abc[numeroABC];
     }
     if (ur.value.includes('qu.ax/') && !ur.value.includes('.mp4')) {
         return list[numero] + '.blogspot.com/' + teg + '?q=' + btoa(ur.value.replace('https://qu.ax/', '').replace('http://qu.ax/', ''));
@@ -241,225 +160,6 @@ function geraAppUrl() {
     }
 
     return list[numero] + '.blogspot.com/' + teg + '?id=' + btoa(ur.value.replace('https://', '').replace('http://', ''));
-}
-
-function getVideos() {
-    var language = (navigator.language || navigator.userLanguage).split('-')[0];
-
-    switch (language) {
-        case 'en':
-            return 'JOIN HERE FOR MORE VIDEOS';
-        case 'es':
-            return 'ÚNETE AQUÍ PARA MÁS VÍDEOS';
-        case 'fr':
-            return 'REJOIGNEZ ICI POUR PLUS DE VIDÉOS';
-        case 'de':
-            return 'TRETEN SIE HIER FÜR MEHR VIDEOS BEI';
-        case 'it':
-            return 'UNISCITI QUI PER ALTRI VIDEO';
-        case 'pt':
-            return 'Assista completo aqui!';
-        case 'ru':
-            return 'ПРИСОЕДИНЯЙТЕСЬ ЗДЕСЬ ДЛЯ БОЛЬШЕ ВИДЕО';
-        case 'zh':
-            return '点击此处加入以获取更多视频';
-        case 'ja':
-            return 'もっとビデオを見るためにここに参加してください';
-        case 'ko':
-            return '더 많은 비디오를 보려면 여기에 가입하세요';
-        case 'ar':
-            return 'انضم هنا للمزيد من مقاطع الفيديو';
-        case 'hi':
-            return 'अध&#2367;क व&#2368;ड&#2367;य&#2379; क&#2375; ल&#2367;ए यह&#2366;&#2305; श&#2366;म&#2367;ल ह&#2379;&#2306;';
-        case 'nl':
-            return 'VOEG JE HIER TOE VOOR MEER VIDEO\'S';
-        case 'pl':
-            return 'DOŁĄCZ TUTAJ, ABY ZOBACZYĆ WIĘCEJ FILMÓW';
-        case 'tr':
-            return 'DAHA FAZLA VİDEO İÇİN BURAYA KATIL';
-        case 'vi':
-            return 'THAM GIA ĐÂY ĐỂ XEM THÊM VIDEO';
-        case 'th':
-            return 'เข&#3657;าร&#3656;วมท&#3637;&#3656;น&#3637;&#3656;สำหร&#3633;บว&#3636;ด&#3637;โอเพ&#3636;&#3656;มเต&#3636;ม';
-        case 'cs':
-            return 'PŘIDEJTE SE SEM PRO VÍCE VIDEÍ';
-        case 'sv':
-            return 'GÅ MED HÄR FÖR FLER VIDEOR';
-        case 'da':
-            return 'Deltag her for flere videoer';
-        case 'no':
-            return 'BLI MED HER FOR FLERE VIDEOER';
-        case 'fi':
-            return 'LIITY TÄHÄN LISÄÄ VIDEOITA VARTEN';
-        case 'hu':
-            return 'CSATLAKOZZ ITT TÖBB VIDEÓÉRT';
-        case 'el':
-            return 'ΣΥΜΜΕΤΕΣΤΕ ΕΔΩ ΓΙΑ ΠΕΡΙΣΣΟΤΕΡΑ ΒΙΝΤΕΟ';
-        case 'uk':
-            return 'ПРИЙМАЙТЕ УЧАСТЬ ТУТ ДЛЯ БІЛЬШЕ ВІДЕО';
-        case 'he':
-            return 'הצטרף כאן לעוד סרטונים';
-        case 'id':
-            return 'BERGABUNG DI SINI UNTUK LEBIH BANYAK VIDEO';
-        case 'ms':
-            return 'SERTAI DI SINI UNTUK LEBIH VIDEO';
-        case 'tl':
-            return 'SUMALI DITO PARA SA HIGIT PANG MGA BIDEO';
-        case 'fil':
-            return 'SUMALI DITO PARA SA HIGIT PANG MGA BIDEO';
-        case 'ceb':
-            return 'MOSALI DINHI PARA SA DAGHANG MGA BIDEO';
-        case 'ja':
-            return 'もっとビデオを見るためにここに参加してください';
-        case 'ko':
-            return '더 많은 비디오를 보려면 여기에 가입하세요';
-        case 'ru':
-            return 'ПРИСОЕДИНЯЙТЕСЬ ЗДЕСЬ ДЛЯ БОЛЬШЕ ВИДЕО';
-        case 'vi':
-            return 'THAM GIA ĐÂY ĐỂ XEM THÊM VIDEO';
-        case 'th':
-            return 'เข&#3657;าร&#3656;วมท&#3637;&#3656;น&#3637;&#3656;สำหร&#3633;บว&#3636;ด&#3637;โอเพ&#3636;&#3656;มเต&#3636;ม';
-        case 'cs':
-            return 'PŘIDEJTE SE SEM PRO VÍCE VIDEÍ';
-        case 'sv':
-            return 'GÅ MED HÄR FÖR FLER VIDEOR';
-        case 'da':
-            return 'Deltag her for flere videoer';
-        case 'no':
-            return 'BLI MED HER FOR FLERE VIDEOER';
-        case 'fi':
-            return 'LIITY TÄHÄN LISÄÄ VIDEOITA VARTEN';
-        case 'hu':
-            return 'CSATLAKOZZ ITT TÖBB VIDEÓÉRT';
-        case 'el':
-            return 'ΣΥΜΜΕΤΕΣΤΕ ΕΔΩ ΓΙΑ ΠΕΡΙΣΣΟΤΕΡΑ ΒΙΝΤΕΟ';
-        case 'uk':
-            return 'ПРИЙМАЙТЕ УЧАСТЬ ТУТ ДЛЯ БІЛЬШЕ ВІДЕО';
-        case 'he':
-            return 'הצטרף כאן לעוד סרטונים';
-        case 'id':
-            return 'BERGABUNG DI SINI UNTUK LEBIH BANYAK VIDEO';
-        case 'ms':
-            return 'SERTAI DI SINI UNTUK LEBIH VIDEO';
-        case 'tl':
-            return 'SUMALI DITO PARA SA HIGIT PANG MGA BIDEO';
-        case 'fil':
-            return 'SUMALI DITO PARA SA HIGIT PANG MGA BIDEO';
-        case 'ceb':
-            return 'MOSALI DINHI'
-        default:
-            return 'JOIN HERE FOR MORE VIDEOS';
-    }
-}
-
-// ads
-
-// click pluble
-var tempLisk2 = false;
-if (window.location.href.includes("#click1")) {
-    startLisk2();
-}
-
-var startLisk = true;
-function startLisk2() {
-    if (startLisk == false) {
-        return;
-    }
-    setTimeout(() => {
-        startLisk = false;
-        //tempLisk2 = true;
-    }, 6000);
-}
-
-var ouvinteClickVar = true;
-function ativarClickADS() {
-    document.querySelector('video').addEventListener('play', () => {
-        ouvinteClick(true);
-        console.log('click');
-    });
-
-    document.body.addEventListener('click', (e) => {
-        ouvinteClick(e.target.nodeName);
-    });
-}
-
-function ouvinteClick(e) {
-    if (ouvinteClickVar == false) {
-        return;
-    }
-    ouvinteClickVar = false;
-    if (e != true) {
-        var evento = e;
-        console.log(evento);
-    }
-    //e.preventDefault();
-    const verElemento = () => {
-        if (e == true) {
-            return true;
-        }
-        if (
-            !evento.includes('IMG')
-            &&
-            !evento.includes('A')
-            &&
-            !evento.includes('BUTTON')
-        ) {
-            return true;
-        }
-        return false
-    };
-
-    if (
-        verElemento() == true
-        &&
-        tempLisk2 == false
-        &&
-        !window.location.href.includes("#click")
-    ) {
-        window.location.href = "#click1";
-        startLisk2();
-        AbriLink('aHR0cHM6Ly93aXNzb29ueS5uZXQvNC83NTU5NDE3');
-        console.log('Link click 1')
-    }
-
-    if (
-        verElemento() == true
-        &&
-        tempLisk2 == true
-        &&
-        window.location.href.includes("#click1")
-    ) {
-        window.location.href = "#click2";
-        AbriLink('aHR0cHM6Ly9yZXN0bGVzc2NvbXBlbGRlc2NlbmQuY29tL2d5M3M3ZjJpNW0/a2V5PWNiMWJlOTJkMTBkZTE0M2E2YTIzYWI1YWNmNDFhNzAx');
-        console.log('Link click 2');
-    }
-    setInterval(() => {
-        ouvinteClickVar = true;
-    }, 300);
-}
-
-function AbriLink(url) {
-    console.log('click')
-    var link = document.createElement('a');
-    var id = "Btn" + String(url.slice(-5));
-    link.id = id;
-    link.target = '_blank';
-    link.href = atob(url);
-    document.body.appendChild(link);
-    var l = document.getElementById(id);
-    setTimeout(() => {
-        l.click();
-        l.href = '';
-        setTimeout(() => {
-            l.remove();
-        }, 50);
-    }, 300);
-}
-
-function ads() {
-    var i = ['PGlmcmFtZSBkYXRhLWFhPScyMzMwNDE1JyBzcmM9Jy8vYWQuYS1hZHMuY29tLzIzMzA0MTU/c2l6ZT05NzB4OTAnIHN0eWxlPSd3aWR0aDoxMDAlOyBoZWlnaHQ6YXV0bzsgYm9yZGVyOjBweDsgcGFkZGluZzowOyBvdmVyZmxvdzpoaWRkZW47IGJhY2tncm91bmQtY29sb3I6IHRyYW5zcGFyZW50Oyc+PC9pZnJhbWU+', 'PGRpdiBzdHlsZT0icGFkZGluZzogM3B4OyB3aWR0aDogYXV0bzsgYm94LXNpemluZzogYm9yZGVyLWJveDsiPjxhIGhyZWY9Imh0dHBzOi8vc29maWxtZS5jbGljay8/YWRzPWEtYWRzbnVuaW1vcyI+PGltZyBzcmM9Imh0dHBzOi8vc29maWxtZS5jbGljay9hc3NldHMvZ2lmL3NvZmlsbWUuZ2lmP2FudWNpb1BhZ289dHJ1ZSI+PC9hPjwvZGl2Pg=='];
-    const n = Math.floor(Math.random() * i.length);
-    return atob(i[n]);
 }
 
 // asd banner 300x250
@@ -508,6 +208,7 @@ function adsBannes(banner) {
         }, 1000);
     }
 }
+
 
 function inputSelect() {
     if (!listaURL) {
@@ -577,7 +278,7 @@ function bootonBaneer() {
 }
 
 function ads_start(){
-    let valor = Math.floor(Math.random() * 2);
+    var valor = Math.floor(Math.random() * 2);
     if (valor % 2 ==0){
         return;
     }
